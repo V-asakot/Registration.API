9114e8363283ff82bca177a0a1b069735c4ba44c
Vasakot <vasily.kotkoff@yandex.ru>
2023-06-23 17:56:58 +0300

    Добавьте файлы проекта.

---
+++ Registration.API/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Registration.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+
+    }
+}
